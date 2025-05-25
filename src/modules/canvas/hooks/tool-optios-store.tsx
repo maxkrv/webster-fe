ee47e8fd38d0d10f3e4fd5f160d02e0153cab150
Nikulin Artem <artem.nikulin.0526@gmail.com>
2025-05-25 15:52:46 +0300

    feat(15): add brush

--- src/modules/canvas/hooks/tool-optios-store.tsx
+++ src/modules/canvas/hooks/tool-optios-store.tsx
@@ -7,8 +7,17 @@ type PointerOptions = {
   trailLength: number;
 };
 
+type BrushOptions = {
+  brushColor: string;
+  brushSize: number;
+  brushOpacity: number;
+  brushType: 'round' | 'square' | 'star';
+  brushSpacing: number;
+};
+
 type ToolOptionsState = {
   pointer: PointerOptions;
+  brush: BrushOptions;
   // other tools can be added here
   setToolOptions: <K extends keyof ToolOptionsState>(tool: K, opts: Partial<ToolOptionsState[K]>) => void;
 };
@@ -20,11 +29,23 @@ export const useToolOptionsStore = create<ToolOptionsState>((set) => ({
     showTrail: true,
     trailLength: 50
   },
+  brush: {
+    brushColor: '#000000',
+    brushSize: 24,
+    brushOpacity: 1,
+    brushType: 'round',
+    brushSpacing: 50
+  },
   setToolOptions: (tool, opts) =>
-    set((state) => ({
-      [tool]: {
-        ...state[tool],
-        ...opts
-      }
-    }))
+    set((state) => {
+      const current = state[tool];
+      const isSame = Object.entries(opts).every(([key, value]) => current[key as keyof typeof current] === value);
+      if (isSame) return state;
+      return {
+        [tool]: {
+          ...current,
+          ...opts
+        }
+      };
+    })
 }));
